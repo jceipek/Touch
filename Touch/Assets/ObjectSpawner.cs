27a5618e645bb21d4f6985e94109a40f6923a6aa
Julian Ceipek <julian.ceipek@gmail.com>
2016-04-08 00:55:58 -0700

    Initial commit

---
+++ Touch/Assets/ObjectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    [SerializeField]
+    GameObject _toSpawn;
+    [SerializeField]
+    SteamVR_TrackedObject[] _trackedObjects;
+    [SerializeField]
+    float _separation = 0.4f;
+
+	void Update () {
+        for (int i = _trackedObjects.Length - 1; i >= 0; i--) {
+            var device = SteamVR_Controller.Input((int)_trackedObjects[i].index);
+            var pos = _trackedObjects[i].GetComponent<PickUpController>()._pickupTransform.position;
+            if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger) && (transform.position - pos).magnitude < _separation) {
+                Instantiate(_toSpawn, pos, Quaternion.identity);
+            }
+        }
+	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, _separation);
+    }
+}
---
+++ Touch/Assets/PickUpController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpController : MonoBehaviour {
+
+    [SerializeField] SteamVR_TrackedObject _trackedObject;
+    SteamVR_Controller.Device _device;
+    [SerializeField]
+    float _pickupRadius;
+    bool _isTriggerPressed;
+    public Transform _pickupTransform;
+
+    void Enable()
+    {
+        //_trackedObject = GetComponent<SteamVR_TrackedObject>();
+       
+    }
+
+	// Use this for initialization
+	void Start () {
+         
+    }
+
+    const int MAX_CARRY = 1;
+    Collider[] _pickupResults = new Collider[MAX_CARRY];
+    CanBePickedUp[] _carried = new CanBePickedUp[MAX_CARRY];
+
+	// Update is called once per frame
+	void Update () {
+        _device = SteamVR_Controller.Input((int)_trackedObject.index);
+        _isTriggerPressed = _device.GetPress(SteamVR_Controller.ButtonMask.Trigger);
+        int overlapCount = Physics.OverlapSphereNonAlloc(_pickupTransform.position, _pickupRadius, _pickupResults);
+        for (int i = overlapCount - 1; i >= 0 && _isTriggerPressed; i--)
+        {
+            var currOverlap = _pickupResults[i];
+            var canBePickedUp = currOverlap.GetComponent<CanBePickedUp>();
+            if (canBePickedUp != null)
+            {
+                _carried[i] = canBePickedUp;
+                //Debug.Log("something picked up");
+            }
+            //Debug.Log("stuff");
+        }
+
+        for (int i = _carried.Length - 1; i >= 0 && _isTriggerPressed; i--)
+        {
+            if (_carried[i] != null)
+            {
+                _carried[i].transform.position = _pickupTransform.position;
+                //Debug.Log("moving a thing");
+            }
+        }
+
+        for (int i = _carried.Length - 1; i >= 0 && !_isTriggerPressed; i--)
+        {
+            _carried[i] = null;
+            //Debug.Log("dropping a thing");
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(_pickupTransform.position, _pickupRadius);
+    }
+}
+//SteamVR_Controller.Input(deviceIndex).GetPressDown(SteamVR_Controller.ButtonMask.Trigger)
